3a6cdec6f4e66338a21b774d16a28288310d7a77
Sunil Keswani <keswanisunil98@gmail.com>
2019-03-13 19:48:01 +0530

    13-3-19

--- common/models/Orders.js
+++ common/models/Orders.js
@@ -65,39 +65,57 @@ module.exports = function(Orders) {
     })
 
     Orders.deleteorder = async function(orderId){
-        var promise = new Promise((resolve, reject)=>{
-            let Items = app.models.Items
-            let Customer = app.models.Customer
-            let Stocklog = app.models.Stocklog
-            for(let i=0;i<orderId.length;i++)
-            {
-                Orders.updateAll({id:orderId[i]},{isenabled:0},(err, order)=>{
-                    Orders.find({include:['customer'],where:{id:orderId[i]}},(err, ordercustomer)=>{
-                        let customersdetails = ordercustomer[0].toJSON().customer
-                        let totalamount = customersdetails.totalamount
-                        totalamount -= ordercustomer[0].totalamount
-                        let receivedamount = customersdetails.received
-                        receivedamount -= ordercustomer[0].receivedamount
-                        Customer.updateAll({id:ordercustomer[0].customerId},{totalamount: totalamount,received: receivedamount},(err,orderUpdate)=>{
-                            let Orderitem = app.models.Orderitem
-                            Stocklog.updateAll({orderId:orderId[i]},{isenabled:0},(err, res)=>{
-                                Orderitem.find({include:['item'],where:{orderId:orderId[i]}},(err, orderitem)=>{
-                                    //console.log(orderitem)
-                                    for(let j=0;j<orderitem.length;j++)
-                                    {
-                                        //console.log(orderitem[j].itemId)
-                                        let orderitemdetails = orderitem[j].toJSON().item
-                                        let usedstock = orderitemdetails.usedstock - orderitem[j].quantity
-                                        Items.updateAll({id:orderitem[j].itemId},{usedstock:usedstock},(err, res)=>{
-                                            if(i == orderId.length - 1) resolve("done")
+        let promise = new Promise((resolve, reject)=>{
+            (async function loop() {
+                let Items = app.models.Items
+                let Customer = app.models.Customer
+                let Stocklog = app.models.Stocklog
+                for(let i=0;i<orderId.length;i++)
+                {
+                    await new Promise(resolve1=>{
+                        Orders.updateAll({id:orderId[i]},{isenabled:0},(err, order)=>{
+                            Orders.find({include:['customer'],where:{id:orderId[i]}},(err, ordercustomer)=>{
+                                let customersdetails = ordercustomer[0].toJSON().customer
+                                let totalamount = customersdetails.totalamount
+                                totalamount -= ordercustomer[0].totalamount
+                                let receivedamount = customersdetails.received
+                                receivedamount -= ordercustomer[0].receivedamount
+                                Customer.updateAll({id:ordercustomer[0].customerId},{totalamount: totalamount,received: receivedamount},(err,orderUpdate)=>{
+                                    let Orderitem = app.models.Orderitem
+                                    Stocklog.updateAll({orderId:orderId[i]},{isenabled:0,notes:'Sales Return of Bill No '+ordercustomer[0].billno},(err, res)=>{
+                                        Orderitem.find({where:{orderId:orderId[i]}},(err, orderitem)=>{
+                                            //console.log(orderitem)
+                                            (async function loop1(){
+                                                for(let j=0;j<orderitem.length;j++)
+                                                {
+                                                    await new Promise(resolve2=>{
+                                                        //console.log(orderitem[j].itemId)
+                                                        Items.find({where:{id:orderitem[j].itemId}},(err, item)=>{
+                                                            let usedstock = item[0].usedstock - orderitem[j].quantity
+                                                            Items.updateAll({id:orderitem[j].itemId},{usedstock:usedstock},(err, res)=>{
+                                                                resolve2()
+                                                            })
+                                                        })
+                                                    })
+                                                    .then(resolve2=>{
+                                                        if(j == (orderitem.length-1))
+                                                        {
+                                                            resolve1("done")
+                                                        }
+                                                    })
+                                                }
+                                            })();
                                         })
-                                    }
+                                    })
                                 })
                             })
                         })
                     })
-                })
-            }
+                    .then(resolve1=>{
+                        if(i == orderId.length - 1) resolve("done")
+                    })
+                }
+            })();
         })
         const billNo = await promise
         return billNo
@@ -106,5 +124,115 @@ module.exports = function(Orders) {
         accepts: {arg: 'orderId', type: 'Array'},
         returns: {arg: 'status', type: 'string'},
         "http": {"verb": "post", "path": "/deleteorder"},
+    })
+
+    Orders.usedstockcal = async function(itemId, itemquantity,type){
+        let promise = new Promise((resolve, reject)=>{
+            let Orderitems = app.models.Orderitem
+            let Items = app.models.Items
+            if(type == "sub")
+            {
+                for(let i=0, p = Promise.resolve();i<itemquantity.length;i++)
+                {
+                    p = p.then(_ => new Promise(resolve1 =>
+                        {
+                            Orderitems.find({where:{id:itemId[i]},include:['item']},(err, orderitem)=>{
+                                let itemDetails = orderitem[0].toJSON().item
+                                let usedstock = parseInt(itemDetails.usedstock)
+                                usedstock -= itemquantity[i]
+                                Items.updateAll({id:itemDetails.id},{usedstock:usedstock},(err,itemstatus)=>{
+                                    if(i == (itemquantity.length - 1)) resolve("done")
+                                    else resolve1()
+                                })
+                            })
+                        }
+                    ))
+                }
+            }
+            else if(type == "add")
+            {
+                for(let i=0, p1 = Promise.resolve();i<itemId.length;i++)
+                {
+                    p1 = p1.then(_ => new Promise(resolve1 =>
+                        {
+                            if(itemId[i].itemId != "")
+                            {
+                                if(itemId[i].id == null)
+                                {
+                                    Items.find({where:{id:itemId[i].itemId}},(err, item)=>{
+                                        let usedstock = parseInt(item[0].usedstock)
+                                        usedstock += itemId[i].quantity
+                                        Items.updateAll({id:itemId[i].itemId},{usedstock:usedstock},(err,itemstatus)=>{
+                                            if(i == (itemId.length - 1)) resolve("done")
+                                            else resolve1()
+                                        })
+                                    })
+                                }
+                                else {
+                                    if(i == (itemId.length - 1)) resolve("done")
+                                    else resolve1()
+                                }
+                            }
+                            else{
+                                if(i == (itemId.length - 1)) resolve("done")
+                                else resolve1()
+                            }
+                        }
+                    ))
+                }
+            }
+            else if(type == "update")
+            {
+                for(let i=0, p1 = Promise.resolve();i<itemId.length;i++)
+                {
+                    p1 = p1.then(_ => new Promise(resolve1 =>
+                        {
+                            if(itemId[i].itemId != "")
+                            {
+                                if(itemId[i].id != null)
+                                {
+                                    Orderitems.find({where:{id:itemId[i].id},include:['item']},(err, orderitem)=>{
+                                        let itemDetails = orderitem[0].toJSON().item
+                                        let usedstock = parseInt(itemDetails.usedstock)
+                                        let stock = parseInt(itemId[i].quantity) - parseInt(orderitem[0].quantity)
+                                        usedstock += stock
+                                        Items.updateAll({id:itemDetails.id},{usedstock:usedstock},(err,itemstatus)=>{
+                                            Orderitems.updateAll({id:itemId[i].id},{
+                                                itemId : itemId[i].itemId,
+                                                quantity : itemId[i].quantity,
+                                                itemprice : itemId[i].mrp,
+                                                taxprice : itemId[i].taxAmount
+                                            },(err, res)=>{
+                                                if(i == (itemId.length - 1)) resolve("done")
+                                                else resolve1()
+                                            })
+                                        })
+                                    })
+                                }
+                                else {
+                                    if(i == (itemId.length - 1)) resolve("done")
+                                    else resolve1()
+                                }
+                            }
+                            else{
+                                if(i == (itemId.length - 1)) resolve("done")
+                                else resolve1()
+                            }
+                        }
+                    ))
+                }
+            }
+        })
+        const billNo = await promise
+        return billNo
+    }
+    Orders.remoteMethod('usedstockcal',{
+        accepts: [
+                    {arg: 'itemId', type: 'Array'},
+                    {arg: 'itemquantity', type: 'Array'},
+                    {arg: 'type', type:'string'}
+                ],
+        returns: {arg: 'status', type: 'string'},
+        "http": {"verb": "post", "path": "/usedstockcal"},
     })
 };
--- common/models/Purchase.js
+++ common/models/Purchase.js
@@ -12,26 +12,33 @@ module.exports = function(Purchase) {
   });
   Purchase.deletepurchase = async function(purchaseId){
       var promise = new Promise((resolve, reject)=>{
-          let Stocklog = app.models.Stocklog
-          let Supplier = app.models.Supplier
-          for(let i=0;i<purchaseId.length;i++)
-          {
-              Purchase.updateAll({id:purchaseId[i]},{isenabled:0},(err, order)=>{
-                  Purchase.find({include:['supplier'],where:{id:purchaseId[i]}},(err, purchasesupplier)=>{
-                      //console.log(purchasesupplier)
-                      let supplierdetails = purchasesupplier[0].toJSON().supplier
-                      let totalamount = supplierdetails.totalamount
-                      totalamount -= purchasesupplier[0].totalamount
-                      let paidamount = supplierdetails.paid
-                      paidamount -= purchasesupplier[0].paidamount
-                      Supplier.updateAll({id:purchasesupplier[0].supplierId},{totalamount: totalamount,paid: paidamount},(err,purchaseUpdate)=>{
-                        Stocklog.updateAll({purchaseId:purchaseId[i]},{isenabled:0},(err, res)=>{
-                          if(i == purchaseId.length - 1) resolve("done")
+          (async function loop() {
+            let Stocklog = app.models.Stocklog
+            let Supplier = app.models.Supplier
+            for(let i=0;i<purchaseId.length;i++)
+            {
+                await new Promise(resolve1=>{
+                  Purchase.updateAll({id:purchaseId[i]},{isenabled:0},(err, order)=>{
+                    Purchase.find({include:['supplier'],where:{id:purchaseId[i]}},(err, purchasesupplier)=>{
+                        //console.log(purchasesupplier)
+                        let supplierdetails = purchasesupplier[0].toJSON().supplier
+                        let totalamount = supplierdetails.totalamount
+                        totalamount -= purchasesupplier[0].totalamount
+                        let paidamount = supplierdetails.paid
+                        paidamount -= purchasesupplier[0].paidamount
+                        Supplier.updateAll({id:purchasesupplier[0].supplierId},{totalamount: totalamount,paid: paidamount},(err,purchaseUpdate)=>{
+                          Stocklog.updateAll({purchaseId:purchaseId[i]},{isenabled:0},(err, res)=>{
+                            resolve1()
+                          })
                         })
-                      })
+                    })
                   })
-              })
-          }
+                })
+                .then(resolve1=>{
+                    if(i == purchaseId.length - 1) resolve("done")
+                })
+            }
+        })();
       })
       //console.log(promise)
       const billNo = await promise
